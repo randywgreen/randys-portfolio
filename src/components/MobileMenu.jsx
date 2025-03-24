827813ee11de790ebbca2af46a9ddc1aba1084b1
Randy Green <randywgreen@gmail.com>
2025-03-24 09:29:17 -0500

    Initial updates

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -4,14 +4,14 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
   return (
     <div
       className={`fixed top-0 left-0 w-full bg-[rgba(10,10,10,0.8)] z-40 flex flex-col items-center justify-center
-                     transition-all duration-300 ease-in-out
+        transition-all duration-300 ease-in-out
 
-                     ${
-                       menuOpen
-                         ? "h-screen opacity-100 pointer-events-auto"
-                         : "h-0 opacity-0 pointer-events-none"
-                     }
-                   `}
+        ${
+          menuOpen
+            ? "h-screen opacity-100 pointer-events-auto"
+            : "h-0 opacity-0 pointer-events-none"
+        }
+      `}
     >
       <button
         onClick={() => setMenuOpen(false)}
